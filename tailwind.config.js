21ac05ba04608616b4eee206b604e0ce99f612ae
mamorshedalam <ma.morshedalampro@gmail.com>
2024-03-02 18:37:14 +0800

    all page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       serif: ["ui-serif", "Georgia"],
       mono: ["ui-monospace", "SFMono-Regular"],
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
   },
   plugins: [],
 };
